117c69190f2153c993be01b62e36dd5318351b43
sara-builds <lucidtechhub@gmail.com>
2025-01-22 21:57:57 -0800

    Initial CDK App

---
+++ test/populate_data_in_ddb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PopulateDataInDdb from '../lib/populate_data_in_ddb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/populate_data_in_ddb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PopulateDataInDdb.PopulateDataInDdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
